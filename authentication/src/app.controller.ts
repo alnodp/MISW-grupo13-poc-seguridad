3c1074d86c682cb2f9b324b2e7b1bcae6157e1d9
Andres Lopez <am.lopezr1@uniandes.edu.co>
2022-11-26 10:59:20 -0500

    creando login y token en el compopnnete de autenticación

--- authentication/src/app.controller.ts
+++ authentication/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(['', 'health'])
+  health(): string {
+    return 'Service healthy';
   }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    const token = this.authService.login(req.user);
+    return {
+      ...req.user,
+      'access-token': token,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
 }
